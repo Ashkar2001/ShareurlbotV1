2d3051a3b5ed424cea97dcf430140d0faff4d0b5
Charlie_jin <64459326+Ashkar2001@users.noreply.github.com>
2021-04-07 22:03:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    APP_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
